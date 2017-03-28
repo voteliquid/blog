26ab94399fa2e5fd6f5fab26b2bf4f5203bed093
David Ernst <git@dsernst.com>
2017-03-28 04:02:23 -0700

    Add target="_blank" to all external links

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,13 +9,13 @@
     $(document).ready(function(){
 
         $(".post-content").fitVids();
-        
+
         // Calculates Reading Time
         $('.post-content').readingTime({
             readingTimeTarget: '.post-reading-time',
             wordCountTarget: '.post-word-count',
         });
-        
+
         // Creates Captions from Alt tags
         $(".post-content img").each(function() {
             // Let's put a caption if there is one
@@ -23,7 +23,21 @@
               $(this).wrap('<figure class="image"></figure>')
               .after('<figcaption>'+$(this).attr("alt")+'</figcaption>');
         });
-        
+
     });
 
 }(jQuery));
+
+
+
+// Adds target="_blank" to all external links
+
+(function() {
+  var links = document.links;
+  for (var i = 0, linksLength = links.length; i < linksLength; i++) {
+    if (links[i].hostname != window.location.hostname) {
+      links[i].target = '_blank';
+      links[i].className += ' externalLink';
+    }
+  }
+})();
